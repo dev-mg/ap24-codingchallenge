ce210626e48629ca5d4f47b462157506c1c141e0
Martin Graf <martin.graf@whataventure.com>
2021-02-06 20:47:05 +0100

    Implement API

--- public/index.php
+++ public/index.php
@@ -6,10 +6,28 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello AP24!");
     return $response;
+});
+
+$app->post('/test/{id}', function (Request $request, Response $response, array $args) {
+	$id = $args['id'];
+	
+	$_input = $request->getParsedBody();
+    $mileage = $_input['mileage'];
+    $preowners = $_input['preowners'];
+    
+	$data = array(
+		'id' => $id,
+		'mileage' => $mileage
+	);
+
+    return $response->withJson($data, 200);
 });
 
 $app->run();
