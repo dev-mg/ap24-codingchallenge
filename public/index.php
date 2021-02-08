aedd67554664252c76072b58b708cd7f32b938bd
Martin Graf <martin.graf@whataventure.com>
2021-02-08 22:51:46 +0100

    Implement validation

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
+
+use DavidePastore\Slim\Validation\Validation;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator as v;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -10,22 +14,40 @@ $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
+$idRules = v::alnum()->noWhitespace()->length(3, 10);
+$mileageRules = v::number()->positive();
+$preownersRules = v::number()->between(1, 3);
+
+$carValidator = v::attribute('id', $idRules)
+    ->attribute('mileage', $mileageRules)
+    ->attribute('preowners', $preownersRules);
+
+$app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello AP24!");
     return $response;
 });
 
-$app->post('/test/{id}', function (Request $request, Response $response, array $args) {
-	$id = $args['id'];
-	
-	$_input = $request->getParsedBody();
-    $mileage = $_input['mileage'];
-    $preowners = $_input['preowners'];
-    
-	$data = array(
-		'id' => $id,
-		'mileage' => $mileage
-	);
+$app->post('/test/{id}', function (Request $request, Response $response, array $args) use ($carValidator) {
+    $input = $request->getParsedBody();
+
+    $car = new stdClass();
+    $car->id = $args['id'];
+    $car->mileage = $input['mileage'];
+    $car->preowners = $input['preowners'];
+
+    try {
+        $carValidator->assert($car);
+    } catch (NestedValidationException $exception) {
+        $data = array(
+            'errors' => $exception->getMessages()
+        );
+        return $response->withJson($data, 422);
+    }
+
+    $data = array(
+        'id' => $car->id,
+        'mileage' => $car->mileage
+    );
 
     return $response->withJson($data, 200);
 });
